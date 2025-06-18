ba4c6aeba8cfab5d9d2e803f09d3d1c55e697f68
MRRegner <rodriguez.regner@gmail.com>
2025-06-17 23:08:29 -0300

    Añadido control de roles para las vistas show en index.php

--- index.php
+++ index.php
@@ -15,14 +15,31 @@ try {
    $controller = (isset($_GET["controller"])) ? $_GET["controller"] : null;
    $method = (isset($_GET["method"])) ? $_GET["method"] : null;
 
-
     //Si no esta logueado o no se esta registrando se tiene que loguear
     if(!isset($_SESSION["user_name"]) && $_GET["controller"] !== 'usuario'){
         $controller = "usuario";
         $method = "showLoginForm";
     }
 
+    /* Control de rol*/
 
+    //Roles: 1 Admin, 2 Editor , 3 Jugador
+    $rol = (isset($_SESSION["id_rol"])) ? $_SESSION["id_rol"] : null;
+
+    //Admin, editor y jugar tienen sus propias vistas show
+    if($controller == 'home' && $method == 'showAdmin' && $rol != 1){
+        $method = $rol == 2 ? 'showEditor' : 'show';
+    }
+
+    if($controller == 'home' && $method == 'showEditor' && $rol != 2){
+        $method = $rol == 1 ? 'showAdmin' : 'show';
+    }
+
+    if($controller == 'home' && $method == 'show' && $rol != 3){
+        $method = $rol == 1 ? 'showAdmin' : 'showEditor';
+    }
+
+    /* Fin de Control de rol */
 
     $router->go($controller, $method);
 
