23665c3e8d0e2135d3eb220a0b9ab80041d7b633
nseehofer <nicolasseehofer.99@hotmail.com>
2025-06-20 19:03:21 -0300

    Nuevos cambios para gestionar la carga de nuevas preguntas por usuarios jugadores

--- controller/HomeController.php
+++ controller/HomeController.php
@@ -50,4 +50,29 @@ class HomeController
         ];
         $this->view->render("partida", $viewData); //
     }
+
+    public function questionRequest() {
+        //$_SESSION['user_id']
+        $viewData = [
+            'usuario' => $_SESSION['user_name'] ?? '',
+            'logo_url' => '/public/img/LogoQuizCode.png',
+            'foto_perfil' => $_SESSION['foto_perfil'],
+            'id_usuario' => $_SESSION['user_id']
+        ];
+        $this->view->render("solicitudPregunta", $viewData);
+    }
+
+    public function requestSubmit() {
+       $idCategoriaSeleccionada =  $_POST['categoria'] ?? null;
+
+       $respuestaCorrecta = $_POST['repuesta_correcta'] ?? null;
+
+       $respuestas = $_POST['respuestas'] ?? [];
+
+       //SE RECIBEN BIEN
+       var_dump($idCategoriaSeleccionada);
+        var_dump($respuestaCorrecta);
+        var_dump($respuestas);
+
+    }
 }
